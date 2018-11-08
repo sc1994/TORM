3b6b73360177a415081eeb5d4e0549dfffcc6615
孙诚 <sunchengsuncheng@gmail.com>
2018-11-08 16:04:41 +0800

    加入 表达式

--- MSORM/MSORM.cs
+++ MSORM/MSORM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace MSORM
@@ -7,209 +8,49 @@ namespace MSORM
     public class MSORM<T> : IMSORM<T>
     {
 
-        public IWhere<T> Eq<TValue>(Expression<Func<T, TValue>> exp, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> Equal<TValue>(Expression<Func<T, TValue>> exp, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> Gt<TValue>(Expression<Func<T, TValue>> exp, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> GreatThan<TValue>(Expression<Func<T, TValue>> exp, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> Gte<TValue>(Expression<Func<T, TValue>> exp, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> GreatThanEqual<TValue>(Expression<Func<T, TValue>> exp, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> Lt<TValue>(Expression<Func<T, TValue>> exp, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> LessThan<TValue>(Expression<Func<T, TValue>> exp, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> Lte<TValue>(Expression<Func<T, TValue>> exp, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> LessThanEqual<TValue>(Expression<Func<T, TValue>> exp, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> Like<TValue>(Expression<Func<T, TValue>> exp, TValue value, LikeEnum way)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> In<TValue>(Expression<Func<T, TValue>> exp, IList<TValue> value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> OrEq<TValue>(Expression<Func<T, TValue>> exp, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> OrEqual<TValue>(Expression<Func<T, TValue>> exp, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> OrGt<TValue>(Expression<Func<T, TValue>> exp, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> OrGreatThan<TValue>(Expression<Func<T, TValue>> exp, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> OrGte<TValue>(Expression<Func<T, TValue>> exp, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> OrGreatThanEqual<TValue>(Expression<Func<T, TValue>> exp, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> OrLt<TValue>(Expression<Func<T, TValue>> exp, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> OrLessThan<TValue>(Expression<Func<T, TValue>> exp, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> OrLte<TValue>(Expression<Func<T, TValue>> exp, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> OrLessThanEqual<TValue>(Expression<Func<T, TValue>> exp, TValue value)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> OrLike<TValue>(Expression<Func<T, TValue>> exp, TValue value, LikeEnum way)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IWhere<T> OrIn<TValue>(Expression<Func<T, TValue>> exp, IList<TValue> value)
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public IOrder<T> Asc(params Expression<Func<T, object>>[] exps)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IOrder<T> Desc(params Expression<Func<T, object>>[] exps)
-        {
-            throw new NotImplementedException();
-        }
-
-        public ISelect<T> Select(params Expression<Func<T, object>>[] exps)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IJoin<T> Join<TJoin>(Expression<Func<T, object>> exp, Expression<Func<TJoin, object>> expJ, IWhere<TJoin> and = null)
-        {
-            throw new NotImplementedException();
-        }
     }
 
     public interface IMSORM<T> : ISelect<T>
     {
-        //IMSORM<T> Select(params Expression<Func<T, object>>[] exps);
 
-
-        //IMSORM<T> Join<TJoin>(TJoin join, Expression<Func<T, object>> exp, Expression<Func<TJoin, object>> expJ,);
-        //IMSORM<T> Join<TJoin>(TJoin join, Expression<Func<T, object>> exp, Expression<Func<TJoin, object>> expJ, object );
-        //IMSORM<T> LJoin<TLJoin>(Expression<Func<T, object>> exp);
-        //IMSORM<T> RJoin<TRJoin>(Expression<Func<T, object>> exp);
-        //IMSORM<T> FJoin<TFJoin>(Expression<Func<T, object>> exp);
     }
 
     public interface ISelect<T> : IJoin<T>
     {
-        ISelect<T> Select(Expression<Func<T, object>>[] exps);
+        ISelect<T> Select<TValue>(Expression<Func<T, TValue>> exp);
+        ISelect<T> Select<TValue>(Expression<Func<T, TValue[]>> exps);
+        ISelect<T> Select(Expression<Func<T, object[]>> exps);
     }
 
     public interface IJoin<T> : IWhere<T>
     {
-
-        IJoin<T> Join<TJoin>(Expression<Func<T, object>> exp, Expression<Func<TJoin, object>> expJ, IWhere<TJoin> and = null);
-        //IJoin<T> LJoin<TLJoin>(Expression<Func<T, object>> exp);
-        //IJoin<T> RJoin<TRJoin>(Expression<Func<T, object>> exp);
-        //IJoin<T> FJoin<TFJoin>(Expression<Func<T, object>> exp);
+        IJoin<T> Join<TJoin>(Expression<Func<T, TJoin, bool>> exp); // todo 扩展支持 "join a on a.id = id and xxx" and后面的语法
+        IJoin<T> JoinL<TLJoin>(Expression<Func<T, TLJoin, bool>> exp);
+        IJoin<T> JoinR<TRJoin>(Expression<Func<T, TRJoin, bool>> exp);
+        IJoin<T> JoinF<TFJoin>(Expression<Func<T, TFJoin, bool>> exp);
     }
 
     public interface IWhere<T> : IOrder<T>
     {
-        IWhere<T> Eq<TValue>(Expression<Func<T, TValue>> exp, TValue value);
-        IWhere<T> Equal<TValue>(Expression<Func<T, TValue>> exp, TValue value);
-        IWhere<T> Gt<TValue>(Expression<Func<T, TValue>> exp, TValue value);
-        IWhere<T> GreatThan<TValue>(Expression<Func<T, TValue>> exp, TValue value);
-        IWhere<T> Gte<TValue>(Expression<Func<T, TValue>> exp, TValue value);
-        IWhere<T> GreatThanEqual<TValue>(Expression<Func<T, TValue>> exp, TValue value);
-        IWhere<T> Lt<TValue>(Expression<Func<T, TValue>> exp, TValue value);
-        IWhere<T> LessThan<TValue>(Expression<Func<T, TValue>> exp, TValue value);
-        IWhere<T> Lte<TValue>(Expression<Func<T, TValue>> exp, TValue value);
-        IWhere<T> LessThanEqual<TValue>(Expression<Func<T, TValue>> exp, TValue value);
-        IWhere<T> Like<TValue>(Expression<Func<T, TValue>> exp, TValue value, LikeEnum way);
-        IWhere<T> In<TValue>(Expression<Func<T, TValue>> exp, IList<TValue> value);
-
-        IWhere<T> OrEq<TValue>(Expression<Func<T, TValue>> exp, TValue value);
-        IWhere<T> OrEqual<TValue>(Expression<Func<T, TValue>> exp, TValue value);
-        IWhere<T> OrGt<TValue>(Expression<Func<T, TValue>> exp, TValue value);
-        IWhere<T> OrGreatThan<TValue>(Expression<Func<T, TValue>> exp, TValue value);
-        IWhere<T> OrGte<TValue>(Expression<Func<T, TValue>> exp, TValue value);
-        IWhere<T> OrGreatThanEqual<TValue>(Expression<Func<T, TValue>> exp, TValue value);
-        IWhere<T> OrLt<TValue>(Expression<Func<T, TValue>> exp, TValue value);
-        IWhere<T> OrLessThan<TValue>(Expression<Func<T, TValue>> exp, TValue value);
-        IWhere<T> OrLte<TValue>(Expression<Func<T, TValue>> exp, TValue value);
-        IWhere<T> OrLessThanEqual<TValue>(Expression<Func<T, TValue>> exp, TValue value);
-        IWhere<T> OrLike<TValue>(Expression<Func<T, TValue>> exp, TValue value, LikeEnum way);
-        IWhere<T> OrIn<TValue>(Expression<Func<T, TValue>> exp, IList<TValue> value);
-
-
+        IWhere<T> Where<TValue>(Expression<Func<T, bool>> exp);
+        IWhere<T> Where<TValue>(Expression<Func<T, bool[]>> exp);
+        IWhere<T> In<TValue>(Expression<Func<T, TValue>> exp, IEnumerable<TValue> values);
+        IWhere<T> NotIn<TValue>(Expression<Func<T, TValue>> exp, IEnumerable<TValue> values);
+        IWhere<T> LikeF<TValue>(Expression<Func<T, TValue>> exp, TValue value);
+        IWhere<T> LikeL<TValue>(Expression<Func<T, TValue>> exp, TValue value);
+        IWhere<T> LikeR<TValue>(Expression<Func<T, TValue>> exp, TValue value);
     }
 
-    public interface IOrder<T>
+    public interface IOrder<T> : IMethod<T>
     {
-        IOrder<T> Asc(params Expression<Func<T, object>>[] exps);
-        IOrder<T> Desc(params Expression<Func<T, object>>[] exps);
+        IOrder<T> OrderA(params Expression<Func<T, object>>[] exps);
+        IOrder<T> OrderD(params Expression<Func<T, object>>[] exps);
+    }
+
+    public interface IMethod<T>
+    {
+        IEnumerable<T> List();
+        bool Exist();
     }
 
 
@@ -217,16 +58,25 @@ namespace MSORM
     {
         public string Name { get; set; }
 
+        public long Id { get; set; }
+
         public void Xxx()
         {
-            new MSORM<Test>()
-                .Select(x => x.Name)
-                .Join<Test2>(x => x.Name, x => x.Name2)
-                .Eq(x => x.Name, "11").OrLt(x => x.Name, "123")
-                .Asc(x => x.Name, x => x.Name)
-                .Desc(x => x.Name);
+            //var t = new MSORM<Test>()
+            //    .Select(x => new object[] { x.Name, x.Id })
+            //    .Join<Test2>(x => x.Name, x => x.Name2)
+
+            //    //.Join<Test2>((x, y) => x.Name == y.Name2)
+
+            //    //.Eq(x => new[] { x.Name == "11", x.Name == "22" })
+            //    .OrLt(x => x.Name, "123")
+            //    .Asc(x => x.Name, x => x.Name)
+            //    .Desc(x => x.Name)
+            //    .List();
         }
     }
+
+
 
     public class Test2
     {
